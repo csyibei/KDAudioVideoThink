5012fe45899fae81aeb1f29a8e4876c04ada2580
csyibei <csyibei_120@163.com>
2017-09-22 20:43:51 +0800

    no message

--- KDCamera/KDCamera/KDRecordViewController.m
+++ KDCamera/KDCamera/KDRecordViewController.m
@@ -19,6 +19,8 @@
 @property (nonatomic,strong) AVCaptureConnection *videoConnection;
 @property (nonatomic,strong) AVCaptureConnection *audioConnection;
 @property (nonatomic,strong) AVAssetWriter *assetWriter;
+@property (nonatomic,strong) AVAssetWriterInput *videoWriteInput;
+@property (nonatomic,strong) AVAssetWriterInput *audioWriteInput;
 @end
 
 @implementation KDRecordViewController
